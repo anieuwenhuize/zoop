f65f6f6b754d33def85c6ab7cbc55e1314783d8b
Andries Nieuwenhuize <nieu0082@hz.nl>
2019-08-13 20:49:48 +0200

    Initial commit: solution with ignore-file

---
+++ zoop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace zoop
+{
+    public class Program
+    {
+
+        public string ShowWelcome()
+        {
+            string msg =  " *** WELCOME at the ***\n"
+                + "three PENGUINS\n" 
+                + "and a POLARBEAR\n"
+                + "ZOO";
+
+            return msg;
+        }
+
+        static void Main(string[] args)
+        {
+            Program zoo = new Program();
+
+            // Start with message
+            Console.WriteLine(zoo.ShowWelcome());
+        }
+    }
+}
