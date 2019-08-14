9b75e135c5bd18d3b0478e089a55d74153c11989
Andries Nieuwenhuize <nieu0082@hz.nl>
2019-08-14 10:39:56 +0200

    Penguins can bee feed

--- zoop/Program.cs
+++ zoop/Program.cs
@@ -4,6 +4,16 @@ namespace zoop
 {
     public class Program
     {
+        int penguinOneEngergy;
+        int penguinTwoEngergy;
+        int penguinThreeEngergy;
+
+        public Program()
+        {
+            penguinOneEngergy = 100;
+            penguinTwoEngergy = 100;
+            penguinThreeEngergy = 100;
+        }
 
         public void ShowWelcome()
         {
@@ -21,6 +31,29 @@ namespace zoop
         public static void Say(String msg)
         {
             Console.WriteLine(msg);
+        }
+
+        public void FeedPenguinOne(int energy)
+        {
+            this.penguinOneEngergy += energy;
+            Say("Penguin 1 feeded.");
+        }
+
+        public void FeedPenguinTwo(int energy)
+        {
+            this.penguinTwoEngergy += energy;
+            Say("Penguin 2 feeded.");
+        }
+
+        public void FeedPenguinThree(int energy)
+        {
+            this.penguinThreeEngergy += energy;
+            Say("Penguin 3 feeded.");
+        }
+
+        public void ShowNoPenguinFeeded()
+        {
+            Say("The zoo caretaker ate the fish himself.");
         }
 
         public void ShowGoodBye()
@@ -41,6 +74,30 @@ namespace zoop
             Environment.Exit(0);
         }
 
+        /**
+         * The zoo caretaker feeds one of the penguins
+         */
+        public void FeedPenguin()
+        {
+            int fish = 50;
+
+            int penguinNumber = GetNumber(1, 3);
+
+            switch(penguinNumber)
+            {
+                case 1: FeedPenguinOne(fish); break;
+                case 2: FeedPenguinTwo(fish); break;
+                case 3: FeedPenguinThree(fish); break;
+                default: ShowNoPenguinFeeded(); break;
+            }
+        }
+
+        public int GetNumber(int from, int to)
+        {
+            var rand = new Random();
+            return rand.Next(from, to);
+        }
+
         static void Main(string[] args)
         {
             Program zoo = new Program();
@@ -58,8 +115,13 @@ namespace zoop
                     // close command
                     case "close": 
                     case "quit":
-                            zoo.CloseApp();
-                            break;
+                        zoo.CloseApp();
+                        break;
+
+                    // feed penguin command
+                    case "feed penguin":
+                        zoo.FeedPenguin();
+                        break;
 
                     // unknown command
                     default:
